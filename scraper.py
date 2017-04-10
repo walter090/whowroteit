099945438eab85bd61b8025d32a28dd5c93fbe98
walter_wu090 <walter.wu090@gmail.com>
2017-04-10 19:45:45 -0400

    argument packing, now can scape multiple poets in one go

--- scraper.py
+++ scraper.py
@@ -4,42 +4,46 @@ import unicodedata
 import os.path
 
 
-def scrape_poet(poet,
-                poet_head='https://www.poetryfoundation.org/poems-and-poets/poets/detail/',
-                poem_head='https://www.poetryfoundation.org/poems-and-poets/poems/detail/',
-                folder='poems/'):
+def scrape_poet(*poets):
 
-    poet_folder = folder + poet + '/'
+    poet_head = 'https://www.poetryfoundation.org/poems-and-poets/poets/detail/'
+    poem_head = 'https://www.poetryfoundation.org/poems-and-poets/poems/detail/'
+    folder = 'poems/'
 
-    if os.path.exists(poet_folder):
-        print '{} already scraped!'.format(poet)
-        return
-    else:
-        os.mkdir(poet_folder)
+    for poet in poets:
+        poet_folder = folder + poet + '/'
 
-    sauce = request.urlopen(poet_head+poet).read()
-    soup = bs.BeautifulSoup(sauce, 'lxml')
-    body = soup.body
+        if os.path.exists(poet_folder):
+            print '{} already scraped!'.format(poet)
+            continue
+        else:
+            os.mkdir(poet_folder)
 
-    links = []
+        sauce = request.urlopen(poet_head+poet).read()
+        soup = bs.BeautifulSoup(sauce, 'lxml')
+        body = soup.body
 
-    for i, poem_section in enumerate(body.find_all('div', class_='detail-assets')):
-        if i:
-            poem_list = poem_section.find('ul', class_='auxiliaryList')
-            for poem in poem_list.find_all('li'):
-                poem_link = poem.find('a').get('href')
-                links.append(poem_link.split('/')[-1])
+        links = []
 
-    for url in links:
-        sauce = request.urlopen(poem_head + url).read()
-        poem_content = bs.BeautifulSoup(sauce, 'lxml').body.find('div', class_='poem')
-        poem = []
-        for verse in poem_content.find_all('div'):
-            ascii_verse = unicodedata.normalize('NFKD', verse.text).encode('ascii', 'ignore')
-            poem.append(ascii_verse)
-        poem_formatted = '\n'.join(poem)
+        for i, poem_section in enumerate(body.find_all('div', class_='detail-assets')):
+            if i:
+                poem_list = poem_section.find('ul', class_='auxiliaryList')
+                for poem in poem_list.find_all('li'):
+                    poem_link = poem.find('a').get('href')
+                    links.append(poem_link.split('/')[-1])
 
-        poem_path = poet_folder+url
-        if not os.path.isfile(poem_path):
-            with open(poem_path, 'w') as f:
-                f.write(poem_formatted)
+        for url in links:
+            sauce = request.urlopen(poem_head + url).read()
+            poem_content = bs.BeautifulSoup(sauce, 'lxml').body.find('div', class_='poem')
+            poem = []
+            for verse in poem_content.find_all('div'):
+                ascii_verse = unicodedata.normalize('NFKD', verse.text).encode('ascii', 'ignore')
+                poem.append(ascii_verse)
+            poem_formatted = '\n'.join(poem)
+
+            poem_path = poet_folder+url
+            if not os.path.isfile(poem_path):
+                with open(poem_path, 'w') as f:
+                    f.write(poem_formatted)
+
+        print '{} added to collection!'.format(poet)
